8a10a7bebbef59f6c99760a3857077cb2f1121e0
User <johnsena322@icloud.com>
2024-10-07 18:14:13 +0400

    Fixes

--- alembic/env.py
+++ alembic/env.py
@@ -1,39 +1,37 @@
 import asyncio
-from logging.config import fileConfig
 
 from alembic import context
-from alembic.script import ScriptDirectory
-from sqlalchemy import pool
+from alembic.config import Config
+from sqlalchemy import URL, MetaData
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import async_engine_from_config
+from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 
-from bot.config_reader import AppConfig, create_app_config
+from bot.config_reader import PostgresConfig
 from bot.db import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-config = context.config
+config: Config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-settings: AppConfig = create_app_config()
-target_metadata = Base.metadata
-config.set_main_option(
-    'sqlalchemy.url',
-    str(settings.postgres.dsn)
-)
+target_metadata: MetaData = Base.metadata
+
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+
+
+def _get_postgres_dsn() -> URL:
+    _config: PostgresConfig = PostgresConfig()
+    return _config.build_dsn()
 
 
 def run_migrations_offline() -> None:
@@ -48,13 +46,11 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url,
+        url=_get_postgres_dsn(),
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
-        process_revision_directives=process_revision_directives,
     )
 
     with context.begin_transaction():
@@ -62,27 +58,18 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(
-        connection=connection,
-        target_metadata=target_metadata,
-        process_revision_directives=process_revision_directives,
-    )
+    context.configure(connection=connection, target_metadata=target_metadata)
 
     with context.begin_transaction():
         context.run_migrations()
 
 
 async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
-    and associate a connection with the context.
-
     """
-
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+    """
+    connectable: AsyncEngine = create_async_engine(url=_get_postgres_dsn())
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
@@ -94,23 +81,6 @@ def run_migrations_online() -> None:
     """Run migrations in 'online' mode."""
 
     asyncio.run(run_async_migrations())
-
-
-def process_revision_directives(context, revision, directives):
-    # extract Migration
-    migration_script = directives[0]
-    # extract current head revision
-    head_revision = ScriptDirectory.from_config(context.config).get_current_head()
-
-    if head_revision is None:
-        # edge case with first migration
-        new_rev_id = 1
-    else:
-        # default branch with incrementation
-        last_rev_id = int(head_revision)
-        new_rev_id = last_rev_id + 1
-    # fill zeros up to 3 digits: 1 -> 001
-    migration_script.rev_id = '{0:03}'.format(new_rev_id)
 
 
 if context.is_offline_mode():
